e4c2e4d9ed17effb7944cb07b2352f0d21304318
cmj <chmin82@gmail.com>
2022-07-25 22:20:45 +0900

    [CHORE] JUnit 5
    - JUnit 5 소개
    - 샘플파일 삭제

--- src/main/java/me/whiteship/inflearnthejavatest/domain/StudyStatus.java
+++
@@ -1,5 +0,0 @@
-package me.whiteship.inflearnthejavatest.domain;
-
-public enum StudyStatus {
-    DRAFT, OPENED, STARTED, ENDED
-}
---
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -0,0 +1,9 @@
+package me.whiteship.inflearnthejavatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+
+
+}
